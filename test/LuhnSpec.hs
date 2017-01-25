20c983dd3fd7f888e1a3fde026a063f3d480d133
Reynaldo Cordero (naldoco) <reynaldo.cordero@gmail.com>
2017-01-25 20:14:50 +0100

    Red. Add sumDigits

--- test/LuhnSpec.hs
+++ test/LuhnSpec.hs
@@ -16,6 +16,13 @@ spec = do
       toDigits 1234567 `shouldBe` [1,2,3,4,5,6,7]
       toDigits 2468 `shouldBe` [2,4,6,8]
   describe "doubleEveryOther" $ do
-    it "doubles every other number starting from the right" $ do
+    it "Doubles every other number starting from the right" $ do
       doubleEveryOther [1,2,3,4,5,6] `shouldBe` [2,2,6,4,10,6]
       doubleEveryOther [2,9,4,5,6] `shouldBe` [2,18,4,10,6]
+  describe "sumDigits" $ do
+    context "When all numbers are less than 10" $
+      it "sums the list of integers" $
+        sumDigits [1..6] `shouldBe` sum [1..6]
+    context "When some numbers are greater than 9" $
+      it "sums their digits first before summing the list" $
+        sumDigits [2,12,4,14,6,8] `shouldBe` 28
